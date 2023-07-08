d7b686a539a7bdc28b995671f95897e93824b665
Kboateng346 <Kboateng346@gmail.com>
2023-07-08 02:59:51 +0000

    commit update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,45 @@
 #include "main.h"
 
 /**
- * binary_to_unsignedint - converts a binary number to an unsigned int
+ * powX - raises b to p
+ * @b: base
+ * @p: power
+ * Return: b the power of p
+ */
+int powX(int b, int p)
+{
+	int prod = 1;
+
+	while (p > 0)
+	{
+		prod *= b;
+		p--;
+	}
+	return (prod);
+}
+
+/**
+ * _len - length of a string
+ * @s:string
+ * Return: lenght of s
+ */
+int _len(const char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
  * @b: input string
  * Return: unsigned decimal conversion, 0 if input is NULL
  */
-unsigned int binary_to_unsignedint(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int power;
 	int num = 0;
