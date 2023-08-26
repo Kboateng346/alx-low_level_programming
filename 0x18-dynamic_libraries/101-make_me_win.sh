4c40390067979a67822095251199e28897059c57
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2023-08-26 02:22:01 +0000

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Kboateng346/alx-low_level_programming/tree/main/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+cp ../sh2.sh .
+. ./sh2.sh
